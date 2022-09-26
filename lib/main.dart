f118b2fb8f8e852a1bfc2e57d3e3e3add0319e49
Srivathsa <bssrivathsa2003@gmail.com>
2022-09-26 21:00:49 +0530

    homePage commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:petvio/page/home-page.dart';
 import 'package:petvio/page/login-page.dart';
+
 import 'routes.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const HomePage(),
-      
+      home: const LoginPage(),
       routes: {
-        Routes.homePage :(context) => const  HomePage(),
+        Routes.homePage: (context) => const HomePage(),
         Routes.loginPage: ((context) => const LoginPage()),
       },
     );
